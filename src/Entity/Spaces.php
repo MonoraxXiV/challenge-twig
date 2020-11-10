d51c57619a64a2c8a2816aa2229eb2831a9ddd61
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-11-10 11:23:12 +0100

    replacing spaces with dashes using str_replace

---
+++ src/Entity/Spaces.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use App\Services\Transform;
+
+class Spaces implements Transform
+{
+    //https://www.w3schools.com/php/php_string.asp
+    public function Transform(string $string)
+    {
+        // TODO: Implement Transform() method.
+        str_replace(" ","-",$_POST['input']);
+    }
+}
