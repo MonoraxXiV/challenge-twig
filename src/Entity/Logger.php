3b60fcb6535f053d6a05f92ebcbff42348970411
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-11-10 16:58:46 +0100

    used file puts content, added resources in list, might need to be moved to services

--- src/Entity/Logger.php
+++ src/Entity/Logger.php
@@ -9,7 +9,7 @@ class Logger
     //https://www.php.net/manual/en/function.file-put-contents.php
     //https://www.w3schools.com/php/func_filesystem_file_put_contents.asp
     //although the projected output seems a bit weird
-    public function Logging()
+    public function Logging($message)
     {
         file_put_contents(__DIR__.'log.info', $_POST['input'], FILE_APPEND );
     }
